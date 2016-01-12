936efdb1ad92fc018c1de131261ba698e4f6a536
Michael Howitz <mh@gocept.com>
2016-01-12 09:57:51 +0100

    Typo.

--- src/gocept/runner/example.py
+++ src/gocept/runner/example.py
@@ -1,5 +1,3 @@
-# Copyright (c) 2008 gocept gmbh & co. kg
-# See also LICENSE.txt
 """Example to demonstrate how to create a "run" script."""
 
 import gocept.runner
@@ -7,5 +5,5 @@ import gocept.runner
 
 @gocept.runner.appmain(ticks=1)
 def example():
-    """Example function. This is called every ticks secons."""
+    """Example function. This is called every ticks seconds."""
     print "Calculating..."

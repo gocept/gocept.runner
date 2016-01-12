574042ba8878e493d21a028c27fb4f0b33501cf4
Michael Howitz <mh@gocept.com>
2016-01-12 10:06:11 +0100

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*names):
     return open(os.path.join(os.path.dirname(__file__), *names), 'r').read()
 
 setup(name='gocept.runner',
-      version='0.8.0.dev0',
+      version='1.0',
       description=
           "Create stand alone programs with full Zope3 runtime environment",
       long_description=(

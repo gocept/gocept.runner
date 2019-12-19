e3c578c0e93afa235c01e4f56ed0104fd9ce693f
Thomas Baumann <thomas.baumann@zeit.de>
2019-12-19 13:36:20 +0100

    ZON-5752: replace ZODB3 by ZODB package in requirements

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     zip_safe=False,
     namespace_packages=['gocept'],
     install_requires=[
-        'ZODB3',
+        'ZODB',
         'decorator',
         'setuptools',
         'transaction',

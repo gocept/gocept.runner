1fdeb465d917be5d0cb493e8ecbd2b11a295a37e
Steffen Allner <sa@gocept.com>
2020-12-23 10:35:09 +0100

    Activate pre-commit and also use it in gha.

--- src/gocept/runner/tests/test_transaction.py
+++ src/gocept/runner/tests/test_transaction.py
@@ -1,15 +1,18 @@
 # Copyright (c) 2011 gocept gmbh & co. kg
 # See also LICENSE.txt
-
-from gocept.runner import transaction_per_item
-import ZODB.POSException
-import gocept.testing.assertion
-import mock
 import unittest
 
+import mock
+import ZODB.POSException
 
-class TransactionPerItemTest(unittest.TestCase,
-                             gocept.testing.assertion.Exceptions):
+import gocept.testing.assertion
+from gocept.runner import transaction_per_item
+
+
+class TransactionPerItemTest(
+    unittest.TestCase,
+    gocept.testing.assertion.Exceptions,
+):
 
     def setUp(self):
         self.calls = mock.Mock()
@@ -35,31 +38,37 @@ class TransactionPerItemTest(unittest.TestCase,
     def test_calls_callables_and_commits_after_each(self):
         self.example_func()
         self.assertEqual(
-            [('step1',),
-             ('commit',),
-             ('step2',),
-             ('commit',)
-             ], self.calls.method_calls)
+            [
+                ('step1',),
+                ('commit',),
+                ('step2',),
+                ('commit',),
+            ], self.calls.method_calls,
+        )
 
     def test_on_exception_calls_abort_and_continues_with_next_item(self):
         self.calls.step1.side_effect = RuntimeError('provoked')
         self.example_func()
         self.assertEqual(
-            [('step1',),
-             ('abort',),
-             ('step2',),
-             ('commit',)
-             ], self.calls.method_calls)
+            [
+                ('step1',),
+                ('abort',),
+                ('step2',),
+                ('commit',),
+            ], self.calls.method_calls,
+        )
 
     def test_conflict_error_during_commit_aborts_and_is_swallowed(self):
         self.calls.commit.side_effect = ZODB.POSException.ConflictError()
         with self.assertNothingRaised():
             self.example_func()
         self.assertEqual(
-            [('step1',),
-             ('commit',),
-             ('abort',),
-             ('step2',),
-             ('commit',),
-             ('abort',),
-             ], self.calls.method_calls)
+            [
+                ('step1',),
+                ('commit',),
+                ('abort',),
+                ('step2',),
+                ('commit',),
+                ('abort',),
+            ], self.calls.method_calls,
+        )
